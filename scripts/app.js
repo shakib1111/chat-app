f417db18025f517c06dd68dd96e05789d72fcdd8
shakib1111 <skshakib0000@gmail.com>
2021-05-08 17:05:39 +0530

    fns cdn link added

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
 const chatList=document.querySelector('.chat-list')
 
 const chatUI=new ChatUI(chatList);
-const chatroom=new Chatroom('gaming','lalit');
+const chatroom=new Chatroom('general','lalit');
 
 chatroom.getChats((data)=>{
     chatUI.render(data);
