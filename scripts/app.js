acf94e784834889a628a28575098571215cd7eb3
shakib1111 <skshakib0000@gmail.com>
2021-05-08 17:24:24 +0530

    chat added to ui

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,17 @@
 
-const chatList=document.querySelector('.chat-list')
+const chatList=document.querySelector('.chat-list');
+const newChatForm=document.querySelector('.new-chat');
+
+
+newChatForm.addEventListener('submit',e=>{
+    e.preventDefault();
+    const message=newChatForm.message.value.trim();
+    chatroom.addChat(message)
+    .then(()=>newChatForm.reset())
+    .catch((err)=>console.log(err));
+
+})
+
 
 const chatUI=new ChatUI(chatList);
 const chatroom=new Chatroom('general','lalit');
