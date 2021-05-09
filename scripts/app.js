388806f0c134ec30732ace889b55a5cb92e8309f
shakib1111 <skshakib0000@gmail.com>
2021-05-09 16:10:38 +0530

    name updated on ui

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 
 const chatList=document.querySelector('.chat-list');
 const newChatForm=document.querySelector('.new-chat');
+const newNameForm=document.querySelector('.new-name');
+const updateMssg=document.querySelector('.update-mssg');
 
 
 newChatForm.addEventListener('submit',e=>{
@@ -10,7 +12,21 @@ newChatForm.addEventListener('submit',e=>{
     .then(()=>newChatForm.reset())
     .catch((err)=>console.log(err));
 
-})
+});
+
+newNameForm.addEventListener('submit',e=>{
+    e.preventDefault();
+    const newName=newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+
+    newNameForm.reset();
+    updateMssg.innerText=`Your Name was updated to ${newName}`;
+    setTimeout(()=> updateMssg.innerText=``,3000);
+});
+
+
+
+
 
 
 const chatUI=new ChatUI(chatList);
